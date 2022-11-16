689cf46a0ca1bd429a991456d3a67ba9a4b1c001
zhoudafeng <1443637646@qq.com>
2022-11-16 14:26:48 +0800

    Initial commit

---
+++ src/main/java/com/ywj/api/utils/JwtUtils.java
@@ -0,0 +1,44 @@
+package com.ywj.api.utils;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * JsonWebToken登录认证
+ */
+@Component
+public class JwtUtils {
+    private long time = 1000 * 60 * 60;
+    public final String signature = "admin";
+
+    public String jwt() {
+        JwtBuilder jwtBuilder = Jwts.builder();
+        String jwtToken = jwtBuilder
+                //添加header头部信息
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("alg", "HS256")
+                //payload(载荷)
+                .claim("username", "tom")
+                .claim("role", "admin")
+                .setSubject("admin-test")
+                .setExpiration(new Date(System.currentTimeMillis() + time))
+                .setId(UUID.randomUUID().toString())
+                //signature
+                .signWith(SignatureAlgorithm.HS256, signature)
+                .compact();
+        return jwtToken;
+    }
+    public void parse(){
+        String token = jwt();
+        JwtParser jwtParser = Jwts.parser();
+        Jws<Claims> headerClaimsJwt = jwtParser.setSigningKey(signature).parseClaimsJws(token);
+        Claims body = headerClaimsJwt.getBody();
+        System.out.println(body.get("username"));
+        System.out.println(body.get("role"));
+        System.out.println(body.getId());
+        System.out.println(body.getSubject());
+        System.out.println(body.getExpiration());
+    }
+}
---
+++ src/test/java/com/ywj/api/AppApplicationTests.java
@@ -0,0 +1,19 @@
+package com.ywj.api;
+
+import com.ywj.api.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class AppApplicationTests {
+    @Autowired
+    JwtUtils jwtUtils;
+    @Test
+    void contextLoads() {
+         log.info("token info:{}",jwtUtils.jwt());
+         jwtUtils.parse();
+    }
+}
